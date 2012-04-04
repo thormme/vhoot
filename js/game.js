607900f98824d3c23e68c7268d6d051cb5e11149
thormme <thormme@yahoo.com>
2012-04-04 07:20:13 +0000

    switched to functional model/inheritance and strict mode

--- js/game.js
+++ js/game.js
@@ -1,122 +1,115 @@
-function Actor (x, y, health, image) {
-    var self = this;
-    
-    self.getX = function() {
-        return x;
-    };
-    
-    self.getY = function() {
-        return y;
-    };
-    
-    self.getHealth = function() {
-        return health;
-    };
-    
-    self.setX = function(newx) {
-        x = newx;
-    };
-    
-    self.setY = function(newy) {
-        y = newy;
-    };
-    
-    self.setHealth = function(newhealth) {
-        health = newhealth;
-    };
-    
-    self.move = function(dx, dy) {
-        x += dx;
-        y += dy;
-    };
-    
-    self.damage = function(dhealth) {
-        health -= dhealth;
-    };
-    
-    self.draw = function(canvas_context) {
-        canvas_context.drawImage(image, x, y);
-    };
-    
-    self.run = function(gamestate, context) {
-		context.drawImage(image, x, y);
-    };
-
-    return self;
-}
-
-function Enemy (x, y, health, image) {
-	var self = new Actor(x, y, health, image);
-	
-	self.run = function(gamestate, context) {
-		y += 1;
-		context.drawImage(image, x, y);
-    };
-	
-	return self;
-}
-
-function Player (x, y, health, image) {
-	var self = new Actor(x, y, health, image);
-	return self;
-}
-
-function GameState () {
-	var self = this;
-	var actors = [];
-	
-	self.addActor = function(actor) {
-		actors.push(actor);
-	};
-	
-	self.getActors = function(actortype) {
-		actortype = typeof actortype !== 'undefined' ? actortype : Object;
-		var acts = [];
-		for(var i=0; i<actors.length; i++) {
-			if(actors[i] instanceof actortype) {
-				acts.push(actors[i]);
+(function () {
+	"use strict";
+	function actor(x, y, health, image, secret) {
+		secret = secret || {};
+		var self = {};
+		secret.x = x;
+		secret.y = y;
+		secret.health = health;
+		secret.image = image;
+		self.getX = function () {
+			return secret.x;
+		};
+	    self.getY = function () {
+	        return secret.y;
+	    };
+	    self.getHealth = function () {
+	        return secret.health;
+	    };
+	    self.setX = function (newx) {
+	        secret.x = newx;
+	    };
+	    self.setY = function (newy) {
+	        secret.y = newy;
+	    };
+	    self.setHealth = function (newhealth) {
+	        health = newhealth;
+	    };
+	    self.move = function (dx, dy) {
+	        secret.x += dx;
+	        secret.y += dy;
+	    };
+	    self.damage = function (dhealth) {
+	        secret.health -= dhealth;
+	    };
+	    self.draw = function (canvas_context) {
+	        canvas_context.drawImage(secret.image, secret.x, secret.y);
+	    };
+	    self.run = function (gamestate, canvas_context) {
+			self.draw(canvas_context);
+	    };
+	    return self;
+	}
+	function enemy(x, y, health, image, secret) {
+		secret = secret || {};
+		var self = actor(x, y, health, image, secret),
+			super_run = self.run;
+		self.run = function (gamestate, canvas_context) {
+			secret.y += 1;
+			super_run(gamestate, canvas_context);
+	    };
+		return self;
+	}
+	function player(x, y, health, image, secret) {
+		secret = secret || {};
+		var self = actor(x, y, health, image, secret),
+			super_run = self.run;
+		self.run = function (gamestate, canvas_context) {
+			secret.y -= 1;
+			super_run(gamestate, canvas_context);
+	    };
+		return self;
+	}
+	function gameState() {
+		var self = {},
+			actors = [];
+		self.addActor = function (actor) {
+			actors.push(actor);
+		};
+		self.getActors = function (actortype) {
+			actortype = typeof actortype !== 'undefined' ? actortype : Object;
+			var acts = [],
+				i;
+			for (i = 0; i < actors.length; i++) {
+				if (actors[i] instanceof actortype) {
+					acts.push(actors[i]);
+				}
 			}
-		}
-		return actors;
-	};
-	
-	self.run = function(gamestate, context) {
-		for(var i=0; i<actors.length; i++) {
-			actors[i].run(gamestate, context);
-		}
-	};
-	
-	return self;
-}
-
-function Game () {
-	var self = this;
-	var canvas = document.getElementById('canvas');
-	var ctx = canvas.getContext('2d');  
-	var images = {};
-	var gamestate = new GameState ();
-	
-	self.init  = function() {
-		images.enemyimg1 = new Image();
-		images.enemyimg1.src = '/thormme/vhoot/workspace/images/redtri.svg';
-		gamestate.addActor(new Enemy(12, 40, 1, images.enemyimg1));
-		
-		setInterval ( self.run, 20 );
-	};
-	
-	self.run = function () {
-		ctx.save();
-		ctx.setTransform(1, 0, 0, 1, 0, 0);
-		ctx.clearRect(0, 0, canvas.width, canvas.height);
-		ctx.restore();
-		gamestate.run(gamestate, ctx);
-	};
-	
-	
-	
-	return self;
-}
-
-var game = new Game();
-game.init();
+			return actors;
+		};
+		self.run = function (gamestate, context) {
+			var i;
+			for (i = 0; i < actors.length; i++) {
+				actors[i].run(gamestate, context);
+			}
+		};
+		return self;
+	}
+	function game() {
+		var self = {},
+			canvas = document.getElementById('canvas'),
+			ctx = canvas.getContext('2d'),
+			images = {},
+			gamestate = gameState();
+		self.init = function () {
+			images.enemyimg1 = new Image();
+			images.enemyimg1.src = '/thormme/vhoot/workspace/images/redtri.svg';
+			images.playerimg1 = new Image();
+			images.playerimg1.src = '/thormme/vhoot/workspace/images/bluetri.svg';
+			gamestate.addActor(enemy(12, 40, 1, images.enemyimg1));
+			gamestate.addActor(player(100, 100, 1, images.playerimg1));
+			setInterval(self.run, 20);
+		};
+		self.run = function () {
+			ctx.save();
+			ctx.setTransform(1, 0, 0, 1, 0, 0);
+			ctx.clearRect(0, 0, canvas.width, canvas.height);
+			ctx.restore();
+			gamestate.run(gamestate, ctx);
+		};
+		return self;
+	}
+	var current_game = game();
+	current_game.init();
+}());
 
